1a8ee8478af871b4e300a39d4003179ce54367b2
Your Name <Prekshithreddy2728@gmail.com>
2026-04-03 07:35:50 +0530

    initial commit: removed large files for successful push

---
+++ TrafficApp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index_root"),
+    path("index.html", views.index, name="index"),
+    path('UserLogin', views.UserLogin, name="UserLogin"),
+	       path('UserLoginAction', views.UserLoginAction, name="UserLoginAction"),	   
+	       path('Signup', views.Signup, name="Signup"),
+	       path('SignupAction', views.SignupAction, name="SignupAction"),
+	       path('TrainYolo', views.TrainYolo, name="TrainYolo"),
+	       path('DetectTraffic', views.DetectTraffic, name="DetectTraffic"),
+	       path('DetectTrafficAction', views.DetectTrafficAction, name="DetectTrafficAction"),	
+	       path('Aboutus', views.Aboutus, name="Aboutus"),
+	       
+]
